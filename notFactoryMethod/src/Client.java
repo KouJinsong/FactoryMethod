8ef19a8e658e566fb2a827dbc1e74a36a700b83e
KouJinsong <85273852+KouJinsong@users.noreply.github.com>
2021-07-01 14:38:52 +0900

    Add files via upload

--- notFactoryMethod/src/Client.java
+++ notFactoryMethod/src/Client.java
@@ -1,39 +1,37 @@
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Seihin[] seihins = new Seihin[3];
         seihins[0] = new Television();
         seihins[1] = new Radio();
         seihins[2] = new Television();
-        for (int i = 0; i < seihins.length; ++i) {
-            if (seihins[i] instanceof Television) {
-                ((Television) seihins[i]).numberring();
-                ((Television) seihins[i]).setDate(Date.today());
-            } else if (seihins[i] instanceof Radio) {
-                ((Radio) seihins[i]).numberring();
+
+        for(int i = 0; i < seihins.length; ++i){
+            if(seihins[i] instanceof Television){
+                ((Television)seihins[i]).numberring();
+                ((Television)seihins[i]).setDate(Date.today());
+            }else if(seihins[i] instanceof Radio){
+                ((Radio)seihins[i]).numberring();
             }
         }
-        for (int i = 0; i < seihins.length; ++i) {
+        for(int i = 0; i < seihins.length; ++i){
             seihins[i].print();
         }
     }
 }
 
-abstract class Seihin {
+abstract class Seihin{
     public abstract void print();
 }
 
-class Television extends Seihin {
+class Television extends Seihin{
     private int tvSerialNumber;
     private String date;
-
     public void numberring() {
         tvSerialNumber = Counter.getTvNumber();
     }
-
     public void setDate(String date) {
         this.date = date;
     }
-
     public void print() {
         System.out.println("このテレビに関する情報:");
         System.out.println(" 製造番号:" + tvSerialNumber);
@@ -41,33 +39,29 @@ class Television extends Seihin {
     }
 }
 
-class Radio extends Seihin {
+class Radio extends Seihin{
     private int radioSerialNumber;
-
     public void numberring() {
         radioSerialNumber = Counter.getRadioNumber();
     }
-
     public void print() {
         System.out.println("このラジオに関する情報:");
         System.out.println(" 製造番号:" + radioSerialNumber);
     }
 }
 
-class Date {
+class Date{
     public static String today() {
         return "2021/07/01";
     }
 }
 
 class Counter {
-    private static int tvNum = 777;
-    private static int radioNum = 888;
-
+    private static int tvNum = 100;
+    private static int radioNum = 76;
     public static int getTvNumber() {
         return tvNum++;
     }
-
     public static int getRadioNumber() {
         return radioNum++;
     }
